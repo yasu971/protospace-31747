b2c2ba5dccaab9f650c844fbdfb8d137d728c729
yasu971 <yasuishii1421@gmail.com>
2020-11-04 17:18:38 +0900

    マイグレーションファイル編集
    ユーザー名、プロフィール、所属、役職を追加した（メールアドレスとパスワードについてはデフォルトでマイグレーションファイルに記載されているため、追記の必要はない）

--- db/migrate/20201104080941_devise_create_users.rb
+++ db/migrate/20201104080941_devise_create_users.rb
@@ -6,6 +6,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :name,               null: false, default: ""
+      t.text   :profile,            null: false, default: ""
+      t.text   :occupation,         null: false, default: ""
+      t.text   :position,           null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
