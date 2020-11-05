1d32a70edc1e4ebd3673b32c3054ec9ae5272e81
yasu971 <yasuishii1421@gmail.com>
2020-11-05 23:24:56 +0900

    マイグレーションファイル適用
    プロトタイプテーブルへ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_04_080941) do
+ActiveRecord::Schema.define(version: 2020_11_05_140059) do
+
+  create_table "prototypes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title"
+    t.text "catch_copy"
+    t.text "concept"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_prototypes_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +36,5 @@ ActiveRecord::Schema.define(version: 2020_11_04_080941) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "prototypes", "users"
 end
