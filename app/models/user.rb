6ed2169a832fd01067decfc63d040dad167960e3
yasu971 <yasuishii1421@gmail.com>
2020-11-05 19:12:13 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :email,    presence: true
-  validates :password, presence: true
+  validates :name,       presence: true
+  validates :profile,    presence: true
+  validates :occupation, presence: true
+  validates :position,   presence: true
 end
