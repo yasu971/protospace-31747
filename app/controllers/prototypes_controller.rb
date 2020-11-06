394273a37fecd7fb086da0cbec57a4331795e619
yasu971 <yasuishii1421@gmail.com>
2020-11-06 19:13:59 +0900

    プロトタイプコントローラー編集

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,6 @@
 class PrototypesController < ApplicationController
   def index
+    # インスタンス変数@prototypesを定義し、すべてのプロトタイプの情報を代入する
   end
 
   def new
@@ -7,5 +8,16 @@ class PrototypesController < ApplicationController
   end
 
   def create
+    @prototype = Prototype.create(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 end
