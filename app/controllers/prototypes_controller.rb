87fa8bf0c4625566fbc43d8704bdf19654abc6cb
yasu971 <yasuishii1421@gmail.com>
2020-11-10 14:41:24 +0900

    ルーティング設定修正
    ネストの導入(親子関係)
    親:プロトタイプ、子:コメント

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -9,6 +9,8 @@ class PrototypesController < ApplicationController
 
   def show
     @prototype = Prototype.find(params[:id])
+
+    
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   get 'prototypes/index'
   root to: "prototypes#index"
+
   resources :prototypes, only: [:index, :new, :create, :show, :edit, :update, :destroy]
-  resources :comments, only: [:create ]
+
+  resources :prototypes do
+    resources :comments, only: :create
+  end
 end
