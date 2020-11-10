008fdad0cdee59e1afb4dcbd1b5dfad38ed870f4
yasu971 <yasuishii1421@gmail.com>
2020-11-10 17:44:37 +0900

    コメントコントローラー編集

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,24 @@
 class CommentsController < ApplicationController
   def create
-
+    if comment = Comment.create(comment_params)
+    # 詳細ページへ
+    redirect_to prototype_comments_path "#{comment.prototype.id}"
+    # redirect_to "/prtotypes/#{comment.prototype.id}"
+  else
+    render :show
   end
+end
+
+  # 参考
+  # if prototype.update(prototype_params)
+  #   redirect_to prototype_path
+  # else
+  #   render :edit
+  # end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+  end
+
+end
