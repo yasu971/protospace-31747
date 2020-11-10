cac41a34b6065883ea2f132551520e511198a491
yasu971 <yasuishii1421@gmail.com>
2020-11-10 10:40:34 +0900

    prototypesコントローラー
    show,edit,update,destroyアクションの設定

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -7,6 +7,30 @@ class PrototypesController < ApplicationController
     @prototype = Prototype.new
   end
 
+  def show
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    prototype = Prototype.find(params[:id])
+    if prototype.update(prototype_params)
+      redirect_to prototype_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    if prototype.destroy
+      redirect_to root_path
+    end
+  end
+
   def create
     @prototype = Prototype.create(prototype_params)
     if @prototype.save
